05b201562bd4842ac326baaca7c53e8dee724083
andriist <andrii.stoikov@gmail.com>
2016-01-31 22:44:07 +0200

    Create GrowBox.py
    The main idea was to create a grow box for internal plant growing with adjustable claimant parameters (light in this case). For such  purposes were used: photocell sensor, capacitor, dc motor, breadboard with some wires, Raspberry pi and a little python. So photocell sensor is connected with capacitor at the breadboard. By time needed to charge the capacitor we can define solar insulation level in order to compare it with optimal range for plant growing. If received data is bigger or smaller then extreme values of solar insulating, dc motor (connected to a jalousie) opens/closes (change the angle of) jalousie until photocell gets optimal amount of light. In perfect scenario humidity sensor, temperature sensor could be used along with humidifier, infrared lamp and ventilator in order to create optimal micro-climate in grow box, but because of luck of equipment and impossibility to test the code I've just left it on paper.

---
+++ GrowBox.py
@@ -0,0 +1,45 @@
+# Maintenance of insolation level at home made grove box. 
+
+import time
+import RPI.GPIO as GPIO
+
+PHOTO_SENSOR_PIN = 22 
+MOTOR_PIN_OPEN = 12 # jaloise operation motor`s GPIO pin
+MOTOR_PIN_CLOSE = 16 # jaloise operation motor`s GPIO pin
+PHOTO_MAX = 50000  # max allowable level of illumination
+PHOTO_MIN = 40000  # min allowable level of illumination
+
+GPIO.setmode(GPIO.BOARD)
+
+# Get data from photo sensor
+def getPhotoSensorData():
+	data = 0;
+	GPIO.setup(PHOTO_SENSOR_PIN, GPIO.OUT)
+	GPIO.output(PHOTO_SENSOR_PIN, LOW)
+	time.sleep(0.1)
+	
+	GPIO.setup(pin, GPIO.IN)
+	while(GPIO.input(PHOTO_SENSOR_PIN) == GPIO.LOW):
+		data += 1 # 
+	return data
+	
+# Run the motor	
+def startMotor(pin):
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, HIGH)
+
+# Stop the motor	
+def stopMotor(pin):
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, LOW)
+			
+while true:
+	photoData = getPhotoSensorData():
+	if photoData > PHOTO_MAX:
+		while (photoData > PHOTO_MAX):
+			startMotor(MOTOR_PIN_CLOSE)
+		stopMotor(MOTOR_PIN_CLOSE)
+	else:
+		while (photoData < PHOTO_MIN):
+			startMotor(MOTOR_PIN_OPEN)
+		stopMotor(MOTOR_PIN_OPEN)
